361f8b531e95560386614a4d4002410a43ca0a71
cyrusnolan <cyrus@cyrusnolan.com>
2024-02-22 15:24:40 -0500

    finished plot() and animate() methods in spacecraft class

--- main.m
+++ main.m
@@ -2,11 +2,14 @@ clc
 clear
 close all
 
+% initialize at equilibrium position and add expectation equilibrium to
+% plot() figures 1 and 2
+
 % parameters
 k_tether = 200;
-b_tether = 20;
-k_truss = 2000;
-b_truss = 200;
+b_tether = 100;
+k_truss = 200;
+b_truss = 100;
 mu = 3.986e14;
 mass_payload = 3000;
 mass_truss = 10;
@@ -23,14 +26,15 @@ true_anomaly = pi/2;
 orbit_sc = orbit(mu,semi_major_axis,eccentricity,inclination, ...
     arugment_of_periapsis,lon_ascending_node,true_anomaly);
 
-% form spacecraft object
+% form spacecraft
 payload_radius = 500;
 truss_length = 250;
-initial_angular_velocity = 0.01;
+initial_angular_velocity = 0.007;
 sc = spacecraft(payload_radius,truss_length,initial_angular_velocity);
 
 % sim spacecraft
-stop_time = 300;
+stop_time = 7000;
 gravity = true;
 sc = sim(sc,stop_time,gravity,parameters_sc,orbit_sc);
 plot(sc)
+animate(sc)
--- spacecraft.m
+++ spacecraft.m
@@ -5,6 +5,10 @@ classdef spacecraft
     properties
         d0
         l0
+        ma
+        mb
+        md
+        me
         BWc0
         BRac0
         BRbc0
@@ -35,7 +39,7 @@ classdef spacecraft
             obj.BVdc0 = zeros(3,1);
             obj.BVec0 = zeros(3,1);
         end
-        
+
         function obj = sim(obj, stopTime, gravity, params, orbit)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
@@ -69,6 +73,10 @@ classdef spacecraft
             NVbo0 = NVbc0+NVco0;
             NVdo0 = NVdc0+NVco0;
             NVeo0 = NVec0+NVco0;
+            obj.ma = params.mP/2;
+            obj.mb = params.mP/2;
+            obj.md = params.mT/6;
+            obj.me = params.mT/6;
             varInit = "StopTime = "+stopTime+"; " + ...
                 "gravity = "+gravity+"; " + ...
                 "k = "+params.k+"; " + ...
@@ -78,14 +86,14 @@ classdef spacecraft
                 "d0 = "+obj.d0+"; " + ...
                 "l0 = "+obj.l0+"; " + ...
                 "mu = "+params.mu+"; " + ...
-                "ma = "+params.mP/2+"; " + ...
-                "ma_inv = "+2/params.mP+"; " + ...
-                "mb = "+params.mP/2+"; " + ...
-                "mb_inv = "+2/params.mP+"; " + ...
-                "md = "+params.mT/6+"; " + ...
-                "md_inv = "+6/params.mT+"; " + ...
-                "me = "+params.mT/6+"; " + ...
-                "me_inv = "+6/params.mT+"; " + ...
+                "ma = "+obj.ma+"; " + ...
+                "ma_inv = "+1/obj.ma+"; " + ...
+                "mb = "+obj.mb+"; " + ...
+                "mb_inv = "+1/obj.mb+"; " + ...
+                "md = "+obj.md+"; " + ...
+                "md_inv = "+1/obj.md+"; " + ...
+                "me = "+obj.me+"; " + ...
+                "me_inv = "+1/obj.me+"; " + ...
                 "NRao0 = "+mat2str(NRao0)+"; " + ...
                 "NRbo0 = "+mat2str(NRbo0)+"; " + ...
                 "NRdo0 = "+mat2str(NRdo0)+"; " + ...
@@ -103,14 +111,170 @@ classdef spacecraft
         end
 
         function plot(obj)
-            figure(1)
-            title("Distance between mass D and E (Truss Length)")
-            t = obj.simout.tout;
-            NRde = obj.simout.NRde;
-            for i=length(NRde):-1:1
-                rde(i) = norm(NRde(i,:));
+            % find extrema
+            NRao = obj.simout.NRao;
+            NRbo = obj.simout.NRbo;
+            for i=length(NRao):-1:1
+                rao = norm(NRao(i,:));
+                rbo = norm(NRbo(i,:));
+                diff(i) = rao-rbo;
             end
-            plot(t,rde)
+            diffMinCond = [false, (diff(2:end-1) > diff(1:end-2)) & (diff(2:end-1) > diff(3:end)), false];
+            diffMaxCond = [false, (diff(2:end-1) < diff(1:end-2)) & (diff(2:end-1) < diff(3:end)), false];
+            diff0Cond = [false, (abs(diff(2:end-1)) < abs(diff(1:end-2))) & (abs(diff(2:end-1)) < abs(diff(3:end))), false];
+            idxMinDiff = find(diffMinCond);
+            idxMaxDiff = find(diffMaxCond);
+            idx0Diff = find(diff0Cond);
+
+            figure(1) % truss length (axes=axL) and tether length (axes=axR) vs time
+            axL = subplot(1,2,1);
+            hold on
+            t = obj.simout.tout;
+            rde = obj.simout.rde;
+            plot(axL,t,rde)
+            plot(axL,t(idxMinDiff),rde(idxMinDiff),'ro',LineWidth=2)
+            plot(axL,t(idxMaxDiff),rde(idxMaxDiff),'bo',LineWidth=2)
+            plot(axL,t(idx0Diff),rde(idx0Diff),'ko',LineWidth=2)
+            title("Distance between mass D and E (Truss Length) vs time")
+            xlabel("t (s)")
+            ylabel("truss length (m)")
+            legend('','local vertical alignment, payload a low','local vertical alignment, payload a high','local horizonal alignment')
+            axR = subplot(1,2,2);
+            hold on
+            plot(axR,t,obj.simout.rad,'b')
+            plot(axR,t,obj.simout.rae,'m-.')
+            plot(axR,t,obj.simout.rbd,'k')
+            plot(axR,t,obj.simout.rbe,'r-.')
+            plot(axR,t(idxMinDiff),obj.simout.rad(idxMinDiff),'ro',LineWidth=2)
+            plot(axR,t(idxMaxDiff),obj.simout.rad(idxMaxDiff),'bo',LineWidth=2)
+            plot(axR,t(idx0Diff),obj.simout.rad(idx0Diff),'ko',LineWidth=2)
+            plot(axR,t(idxMinDiff),obj.simout.rbd(idxMinDiff),'ro',LineWidth=2)
+            plot(axR,t(idxMaxDiff),obj.simout.rbd(idxMaxDiff),'bo',LineWidth=2)
+            plot(axR,t(idx0Diff),obj.simout.rbd(idx0Diff),'ko',LineWidth=2)
+            title("Tether length vs time")
+            xlabel("t (s)")
+            ylabel("tether length (m)")
+            legend('rad','rae','rbd','rbe','local vertical alignment, payload a low','local vertical alignment, payload a high','local horizonal alignment','','','')
+            
+            figure(2)
+            NRac = obj.simout.NRac;
+            NRbc = obj.simout.NRbc;
+            NRdc = obj.simout.NRdc;
+            NRec = obj.simout.NRec;
+            NVac = obj.simout.NVac;
+            NVbc = obj.simout.NVbc;
+            NVdc = obj.simout.NVdc;
+            NVec = obj.simout.NVec;
+            for i=length(NRac):-1:1
+                Hac = obj.ma*cross(NRac(i,:),NVac(i,:));
+                Hbc = obj.mb*cross(NRbc(i,:),NVbc(i,:));
+                Hdc = obj.md*cross(NRdc(i,:),NVdc(i,:));
+                Hec = obj.me*cross(NRec(i,:),NVec(i,:));
+                hc(i) = norm(Hac+Hbc+Hdc+Hec);
+            end
+            ax = axes();
+            hold on
+            plot(ax,t,hc);
+            plot(ax,t(idxMinDiff),hc(idxMinDiff),'ro',LineWidth=2)
+            plot(ax,t(idxMaxDiff),hc(idxMaxDiff),'bo',LineWidth=2)
+            plot(ax,t(idx0Diff),hc(idx0Diff),'ko',LineWidth=2)
+            title("Spacecraft angular momentum")
+            xlabel("t (s)")
+            ylabel("angular momentum (kg*m^2/s)")
+            legend('','local vertical alignment, a low','local vertical alignment, a high','local horizonal alignment')
+
+            figure(3)
+            axL = subplot(1,2,1);
+            title("Spacecraft Attitude Trajectory")
+            xlabel('X')
+            ylabel('Y')
+            zlabel('Z')
+            view(axL,3)
+            data = [NRac;NRbc;NRdc;NRec];
+            axis(getAxisLimits3(data))
+            hold on
+            plot3(axL,NRac(:,1),NRac(:,2),NRac(:,3),'k')
+            plot3(axL,NRbc(:,1),NRbc(:,2),NRbc(:,3),'k')
+            plot3(axL,[NRdc(end,1) NRac(end,1)],[NRdc(end,2) NRac(end,2)],[NRdc(end,3) NRac(end,3)],'b')
+            plot3(axL,[NRec(end,1) NRac(end,1)],[NRec(end,2) NRac(end,2)],[NRec(end,3) NRac(end,3)],'b')
+            plot3(axL,[NRdc(end,1) NRbc(end,1)],[NRdc(end,2) NRbc(end,2)],[NRdc(end,3) NRbc(end,3)],'b')
+            plot3(axL,[NRec(end,1) NRbc(end,1)],[NRec(end,2) NRbc(end,2)],[NRec(end,3) NRbc(end,3)],'b')
+            plot3(axL,[NRdc(end,1) NRec(end,1)],[NRdc(end,2) NRec(end,2)],[NRdc(end,3) NRec(end,3)],'k--')
+            legend('Payload a & b', '', 'tethers', '', '', '', 'truss')
+            axR = subplot(1,2,2);
+            title("Spacecraft Orbit Trajectory")
+            xlabel('X')
+            ylabel('Y')
+            zlabel('Z')
+            view(axR,3)
+            NRdo = obj.simout.NRdo;
+            NReo = obj.simout.NReo;
+            data = [NRdo;NReo];
+            axis(getAxisLimits3(data))
+            hold on
+            plot3(axR,NRdo(:,1),NRdo(:,2),NRdo(:,3))
+            plot3(axR,NReo(:,1),NReo(:,2),NReo(:,3))
+            legend('Truss end (D)','Truss end (E)')
+        end
+
+        function animate(obj)
+            % t = obj.simout.tout;
+            % rounded_values = floor(floor(t));
+            % unique_values = unique(rounded_values);
+            % idx = zeros(length(unique_values),1);
+            % for i = 1:length(unique_values)
+            %     index = find(rounded_values == unique_values(i), 1, 'first');
+            %     idx(i) = index;
+            % end
+            NRac = obj.simout.NRac;
+            NRbc = obj.simout.NRbc;
+            NRdc = obj.simout.NRdc;
+            NRec = obj.simout.NRec;
+            NRco = obj.simout.NRco;
+            figure
+            axL = subplot(1,2,1);
+            title("Spacecraft Attitude Trajectory")
+            xlabel('X')
+            ylabel('Y')
+            zlabel('Z')
+            view(axL,3)
+            data = [NRac;NRbc;NRdc;NRec];
+            axis(getAxisLimits3(data))
+            trace_a = animatedline(axL);
+            trace_b = animatedline(axL);
+            tether_ad = animatedline(axL);
+            tether_ae = animatedline(axL);
+            tether_bd = animatedline(axL);
+            tether_be = animatedline(axL);
+            truss_de = animatedline(axL);
+            axR = subplot(1,2,2);
+            title("Spacecraft Orbit Trajectory")
+            xlabel('X')
+            ylabel('Y')
+            zlabel('Z')
+            view(axR,3)
+            data = NRco;
+            limits = getAxisLimits3(data);
+
+            axis([min(limits) max(limits) min(limits) max(limits) min(limits) max(limits)])
+            truss_center = animatedline(axR);
+            pause;
+            for i=1:20:length(NRac)
+                addpoints(trace_a,NRac(i,1),NRac(i,2),NRac(i,3))
+                addpoints(trace_b,NRbc(i,1),NRbc(i,2),NRbc(i,3))
+                addpoints(tether_ad,[NRdc(i,1) NRac(i,1)],[NRdc(i,2) NRac(i,2)],[NRdc(i,3) NRac(i,3)])
+                addpoints(tether_ae,[NRec(i,1) NRac(i,1)],[NRec(i,2) NRac(i,2)],[NRec(i,3) NRac(i,3)])
+                addpoints(tether_bd,[NRdc(i,1) NRbc(i,1)],[NRdc(i,2) NRbc(i,2)],[NRdc(i,3) NRbc(i,3)])
+                addpoints(tether_be,[NRec(i,1) NRbc(i,1)],[NRec(i,2) NRbc(i,2)],[NRec(i,3) NRbc(i,3)])
+                addpoints(truss_de,[NRdc(i,1) NRec(i,1)],[NRdc(i,2) NRec(i,2)],[NRdc(i,3) NRec(i,3)])
+                addpoints(truss_center,NRco(i,1),NRco(i,2),NRco(i,3))
+                drawnow limitrate
+                clearpoints(tether_ad)
+                clearpoints(tether_ae)
+                clearpoints(tether_bd)
+                clearpoints(tether_be)
+                clearpoints(truss_de)
+            end
         end
     end
 end
